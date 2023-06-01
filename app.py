72d98357abb7afe1ef42c055ac0ede0551b990ea
SamyakJ05 <sj.samyakj@gmail.com>
2023-06-01 05:23:50 +0000

    Load jobs from Database

--- app.py
+++ app.py
@@ -1,42 +1,22 @@
 from flask import Flask, render_template, jsonify
-
+from database import load_jobs_from_db
+from sqlalchemy import text
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id' : 1,
-    'title': 'Data Analyst',
-    'location': 'Bengaluru, India',
-    'salary': 'Rs. 10,00,000'
-  },
-  {
-    'id' : 2,
-    'title': 'Data Scientist',
-    'location': 'Delhi, India',
-    'salary': 'Rs. 15,00,000'
-  },
-  {
-    'id' : 3,
-    'title': 'Frontend Engineer',
-    'location': 'Remote'
-  },
-  {
-    'id' : 4,
-    'title': 'Backend Engineer',
-    'location': 'San Fransico, USA',
-    'salary': '$120,000'
-  }
-]
+
+
 
 @app.route("/")
 def flask_tutorial():
+  jobs_list = load_jobs_from_db()
   return render_template('home.html',
-                        jobs=JOBS,
+                        jobs=jobs_list,
                         company_name='Jovian') 
   
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs_list = load_jobs_from_db()
+  return jsonify(jobs_list)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
