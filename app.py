69f2f2d2674606106b142fcf40e04e8611919a7d
SamyakJ05 <sj.samyakj@gmail.com>
2023-06-01 08:52:36 +0000

    Dynamic Page for each Job

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
-from sqlalchemy import text
+from database import load_jobs_from_db, load_job_from_db
+
 app = Flask(__name__)
 
 
@@ -11,12 +11,20 @@ def flask_tutorial():
   jobs_list = load_jobs_from_db()
   return render_template('home.html',
                         jobs=jobs_list,
-                        company_name='Jovian') 
+                        ) 
   
 @app.route("/api/jobs")
 def list_jobs():
   jobs_list = load_jobs_from_db()
   return jsonify(jobs_list)
+
+@app.route("/job/<ID>")
+def show_job(ID):
+    job = load_job_from_db(ID)
+    if not job:
+      return "Not Found", 404
+    else:
+      return render_template('jobpage.html', job = job)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -19,3 +19,15 @@ def load_jobs_from_db():
     for row in result.all():
       jobs.append(dict(row._mapping))
     return jobs
+
+def load_job_from_db(ID):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("SELECT * FROM jobs WHERE id = :val"),
+      {"val": ID}
+    )
+    rows = result.mappings().all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
